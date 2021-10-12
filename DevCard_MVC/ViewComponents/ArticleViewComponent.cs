8611d633798bcc71492609c87ba23130ba35d18c
Mohammad Rezaei <mohate2433@gmail.com>
2021-10-12 21:49:02 +0330

    Project &Article move to Components

---
+++ DevCard_MVC/ViewComponents/ArticleViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DevCard_MVC.Models;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ArticleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Articles = new List<Article>
+            {
+                new Article (id:1,title:"مقاله اول",description:"این مقاله اول است" ,image :"blog-post-thumb-1.jpg" ),
+                new Article (id:2,title:"مقاله دوم",description:"این مقاله دوم است" ,image :"blog-post-thumb-2.jpg" ),
+                new Article (id:3,title:"مقاله سوم",description:"این مقاله سوم است" ,image :"blog-post-thumb-3.jpg" ),
+                new Article (id:4,title:"مقاله چهارم",description:"این مقاله چهارم است" ,image :"blog-post-thumb-4.jpg" )
+            };
+            return View("_Article", Articles);
+        }
+    }
+}
---
+++ DevCard_MVC/ViewComponents/ProjectViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DevCard_MVC.Models;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Projects = new List<Project>
+            {
+                new Project(id : 1 , name : "پروژه" , description : "پروژه اول" , client : "proj" , image :"Project-1.jpg" ),
+                new Project(id : 2 , name : "پروژه" , description : "پروژه دوم" , client : "proj" , image :"Project-2.jpg" ),
+                new Project(id : 3 , name : "پروژه" , description : "پروژه سوم" , client : "proj" , image :"Project-3.jpg" ),
+                new Project(id : 4 , name : "پروژه" , description : "پروژه چهارم" , client : "proj" , image :"Project-4.jpg" ),
+            };
+            return View("_Project",Projects);
+        }
+    }
+}
