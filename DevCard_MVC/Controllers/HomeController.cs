04f6f83d4660db8124eebec82eb2fe84f6abc2f9
Mohammad Rezaei <mohate2433@gmail.com>
2021-10-14 17:00:39 +0330

    dainamic Contact

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,27 +8,55 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly List<Service> _services = new List<Service>
         {
-            _logger = logger;
-        }
+            new Service(id :  1, name : "نقره ای"),
+            new Service(id :  2, name : "طلایی"),
+            new Service(id :  3, name : "پلاتین"),
+            new Service(id :  4, name : "الماس")
+        };
 
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
-            var model = new Contact();
+            var model = new Contact
+            {
+                Services = new SelectList(_services, dataValueField:"Id",dataTextField:"Name")
+            };
             return View(model);
         }
+        
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            model.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نمیباشد . لطفا دوباره تلاش کنید.";
+                return View(model);
+            }
+            ModelState.Clear();
+
+            model = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+
+            ViewBag.success = "پیغام شما با موفقیت ارسال شد . با تشکر";
+            
+            return View(model);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- DevCard_MVC/Models/Contact.cs
+++ DevCard_MVC/Models/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevCard_MVC.Models
 {
@@ -13,5 +14,6 @@ namespace DevCard_MVC.Models
         public string Email { get; set; }
         public string message { get; set; }
         public string Service { get; set; }
+        public SelectList Services {  get; set; }
     }
 }
